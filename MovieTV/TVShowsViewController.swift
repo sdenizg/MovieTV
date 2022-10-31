539bd0108e10c0ccc1ef1a3be8ded7711cbf16a3
Şerife Deniz Geçginer <61096546+sdenizg@users.noreply.github.com>
2022-10-31 13:35:57 +0300

    V.1
    V.1

---
+++ MovieTV/TVShowsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TVShowsViewController.swift
+//  MovieTV
+//
+//  Created by Ş. Deniz Geçginer on 18.10.2022.
+//
+
+import UIKit
+
+class TVShowsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
